dcbea101a9bdafb2e94d5e1e627d7c4d5f370858
nikan <nikan@MARIANA-PLAYGRO>
2023-07-25 16:59:59 +0800

    添加项目文件。

---
+++ 1.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	//第一个程序
+//	cout << "this is a C++ program.";
+//}
+
+//int main()
+//{
+//	//求a+b两数之和
+//	int a, b, sum;
+//	cin >> a >> b;
+//	sum = a + b;
+//	cout << "sum=" << sum << endl;
+//	return 0;
+//}
+
+////求两数的最大值
+//int max(int a, int b)
+//{
+//	int z;
+//	if (a > b)
+//		z = a;
+//	else
+//		z = b;
+//	return z;
+//}
+//int main()
+//{
+//	int x,y,m;
+//	cin >> x >> y;
+//	m=max(x, y);
+//	cout << "max=" << m << endl;
+//	return 0;
+//}
+
